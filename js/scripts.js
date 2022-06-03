1b375af5c85931938e60d4a8fa58ba040e4b9b27
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-03 11:19:29 -0700

    WIP:adding linear gradient, trying to fix for the repeating

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,14 @@ function progLang(time, speed, pay, sleep, num) {
 };
 
 $(document).ready(function() {
-  $("form#triCalc").submit(function(event) {
+  $("form#prog").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("input#input1").val());
     const number2 = parseInt($("input#input2").val());
     const number3 = parseInt($("input#input3").val());
     const number4 = parseInt($("input#input4").val());
     const number5 = parseInt($("input#input5").val());
-    // updated the arguments below to match the variable names above
+  
     let result = progLang(number1, number2, number3, number4, number5);
      
     $("#output").text(result);
