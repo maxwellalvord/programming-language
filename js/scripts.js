a41e6c4b93dd706bbc0fa0a2ac9127cdca385e3f
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-03 09:27:17 -0700

    WIP:updated scripts so follow the framing of triangle tracker

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
+
+// updated the parameters below to match the names within the function
+function progLang(time, speed, pay, sleep, cream) {
+  /* logic for each if/else still needs to be adjusted, 
+  don't forget to use && / ||to mean 'and' / || */
+  if (time < 30 && speed === 'Fast' && pay > 10 && sleep < 6 && cream === 'Vanilla') {
+    return "C Sharp"; 
+  } else if ((time > 30) && (speed === 'Slow')&& (pay > 10) && (sleep < 6 ) && (cream === 'Vanilla')) {
+    return "Python";
+  } else if (time < 30 && speed === 'Fast') {
+     return "JavaScript";
+  } else /*(age > 30 && speed === 'Fast')*/ {
+    return "C Sharp";
+  }
+};
+
 $(document).ready(function() {
-  
   $("form#matchmaking").submit(function(event) {
-  
+
+    //adjusted the ID names to match the ones on the html (input1/input2/input3)
     const time = parseInt($("input#time").val());
     const speed = $("select#speed").val();
     const pay = parseInt($("input#pay").val());
     const sleep = parseInt($("input#sleep").val());
     const cream = parseInt($("input#cream").val());
+    // updated the arguments below to match the variable names above
+    let result = progLang(time, speed, pay, sleep, cream);
     
-  if (time < 30 && speed === 'Fast' && pay > 10 && sleep < 6 && cream === 'Vanilla') {
-    $("#match").text("C Sharp"); 
-  } else if ((time > 30) && (speed === 'Slow')&& (pay > 10) && (sleep < 6 ) && (cream === 'Vanilla')) {
-    $("#match").text("Python");
-  } else if (time < 30 && speed === 'Fast') {
-     $("#match").text("JavaScript");
-  } else /*(age > 30 && speed === 'Fast')*/ {
-    $("#match").text("C Sharp");
-  }
-  event.preventDefault();
+    $("#output").text(result);
+    event.preventDefault();
   });
-  $("#hide-match").click(function(event) {
-    $("#match").text("");
-    $("#time").val("");
-    $("#speed").val("");
-    $("#pay").val("");
-    
-    $("#sleep").val("");
-    $("#cream").val("");
-  })
 });
