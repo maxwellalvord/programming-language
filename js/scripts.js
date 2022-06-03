e3ab0ef76087bdd68cfdcb4f30062194fbe4ab62
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-03 09:10:21 -0700

    WIP:updating logic in script

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
   
     const time = parseInt($("input#time").val());
     const speed = $("select#speed").val();
+    const pay = parseInt($("input#pay").val());
+    const sleep = parseInt($("input#sleep").val());
+    const cream = parseInt($("input#cream").val());
     
-
-  if (time < 30 && speed === 'Slow') {
+  if (time < 30 && speed === 'Fast' && pay > 10 && sleep < 6 && cream === 'Vanilla') {
     $("#match").text("C Sharp"); 
-  } else if (time > 30 && speed === 'Slow') {
+  } else if ((time > 30) && (speed === 'Slow')&& (pay > 10) && (sleep < 6 ) && (cream === 'Vanilla')) {
     $("#match").text("Python");
   } else if (time < 30 && speed === 'Fast') {
      $("#match").text("JavaScript");
@@ -19,6 +21,11 @@ $(document).ready(function() {
   });
   $("#hide-match").click(function(event) {
     $("#match").text("");
-    $("#age").val("");
+    $("#time").val("");
+    $("#speed").val("");
+    $("#pay").val("");
+    
+    $("#sleep").val("");
+    $("#cream").val("");
   })
 });
